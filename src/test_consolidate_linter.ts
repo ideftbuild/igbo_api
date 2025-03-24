f55eeedeb57333df11945e058a650767750b685b
ideftbuild <a.akanSWE@gmail.com>
2025-03-24 13:10:01 +0100

    test(linter): add function with linter issues

---
+++ src/test_consolidate_linter.ts
@@ -0,0 +1,11 @@
+
+
+export function test_linter(condition) {
+	// Adding some linter errors to test 'Test Suite' workflow
+	if (condition) {
+		console.log(true);
+	}  
+	else {
+		console.log(false);
+	}
+}
